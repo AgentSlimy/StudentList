c3cd4b4cba6c91fffd9c7b437dc1fc18fba10b60
U-BSD\417197 <417197@SHS-10D9C06F2.beaverton.k12.or.us>
2021-10-29 12:11:57 -0700

    Initial push

--- StudentList.cpp
+++ StudentList.cpp
@@ -8,10 +8,58 @@ struct student {
   char lastName[21];
   int studentID;
   float GPA;
-
 };
 
 
 int main() {
+  char action[7];
+  bool running = true;
+  cout << "ADD, PRINT, DELETE, or QUIT" << endl;
+  while (running == true) {
+    cin >> action;
+    if (action[0] == 'A' && action[1] == 'D' && action[2] == 'D') {
+      cout << "Adding new student" << endl;
+
+      running = false;
+    }
+    else if (action[0] == 'P' && action[1] == 'R' && action[2] == 'I' && action[3] == 'N' && action [4] == 'T') {
+      cout << "Printing all students" << endl;
+
+      //running = false;
+    }
+    else if (action[0] == 'D' && action[1] == 'E' && action[2] == 'L' && action[3] == 'E' && action[4] == 'T' && action[5] == 'E') {
+      cout << "You are about to delete a student. Confirm? 'Y' for yes 'N' for no" << endl;
+      char deleteCheck[2];
+      cin >> deleteCheck;
+      bool deleteChecking = true;
+      while (deleteChecking == true) {
+	if (deleteCheck[0] == 'Y') {
+	  cout << "Deleting student" << endl;
+	  deleteChecking = false;
+	  //running = false;
+	}
+	else if (deleteCheck[0] == 'N') {
+	  cout << "Back to main menu" << endl;
+	  cout << "ADD, PRINT, DELETE, or QUIT" << endl;
+	  deleteChecking = false;
+	  running = true;
+	}
+	else {
+	  cout << "Invalid input. Ensure that your response is capitalized and is a 'Y' or 'N'" << endl;
+	  deleteChecking = true;
+	}
+      }
+    }
+    else if (action[0] == 'Q' && action[1] == 'U' && action[2] == 'I' && action[3] == 'T') {
+      cout << "Quitting program" << endl;
+      running = false;
+    }
+    else {
+      cout << "Invalid input. Ensure that all commands are in all capital letters." << endl;
+      running = true;
+    }
+  }
+
+
   
-}
+}//end of main()
