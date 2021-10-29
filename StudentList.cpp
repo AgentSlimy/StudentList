5caecc5b215cb2ff808bfc40bab8515bcfbe3c68
U-BSD\417197 <417197@SHS-10D9C06F2.beaverton.k12.or.us>
2021-10-29 11:31:37 -0700

    test

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct student {
+  char firstName[21];
+  char lastName[21];
+  int studentID;
+  float GPA;
+
+};
+
+
+int main() {
+  
+}
